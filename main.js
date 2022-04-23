5602ad215545fabb0e87dfcb66c99afd8dd448f8
mohosin16103308 <hamza1610330816@gmail.com>
2022-04-23 21:32:31 +0600

    Difference of var and let variable type

--- main.js
+++ main.js
@@ -2,3 +2,19 @@ function log(message) {
     console.log(message);
 }
 log("Hello World");
+// var vs let difference between them ****************
+// var can be accessable for the outer parent but can not be acces without its scope
+function doSomeThingUsingVar() {
+    for (var i = 0; i < 5; i++) {
+        console.log(i);
+    }
+    console.log("Final Value = ".concat(i));
+}
+doSomeThingUsingVar();
+function doSomeThingUsingLet() {
+    for (var i = 0; i < 5; i++) {
+        console.log(i);
+    }
+    console.log("Final Value = ".concat(i));
+}
+doSomeThingUsingLet();
--- main.ts
+++ main.ts
@@ -3,3 +3,25 @@ function log(message) {
 }
 
 log("Hello World");
+
+// var vs let difference between them ****************
+// var can be accessable for the outer parent but can not be acces without its scope
+function doSomeThingUsingVar() {
+  for (var i = 0; i < 5; i++) {
+    console.log(i);
+  }
+
+  console.log(`Final Value = ${i}`);
+}
+
+doSomeThingUsingVar();
+
+function doSomeThingUsingLet() {
+  for (let i = 0; i < 5; i++) {
+    console.log(i);
+  }
+  // We can not access without its scope i because it declared  using let .
+  console.log(`Final Value = ${i}`);
+}
+
+doSomeThingUsingLet();
